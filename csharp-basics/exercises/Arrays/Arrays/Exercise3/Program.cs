ac94dd4f84d8d47da109e8d5a9888c617aaa6d6e
Martins Pavlovskis <martinspavlovskis@yahoo.com>
2021-07-20 22:55:14 +0300

    Revert "Revert "8/10""
    This reverts commit 25f1c4ea362eb85fe74810cea57bf6749bdc6aa6.

--- csharp-basics/exercises/Arrays/Arrays/Exercise3/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise3/Program.cs
@@ -1,24 +1,24 @@
-﻿namespace Exercise3
+﻿using System;
+
+namespace Exercise3
 {
     class Program
-    {
-        //TODO: Write a C# program to calculate the average value of array elements.
+    {        
         private static void Main(string[] args)
         {
-            int[] numbers = {20, 30, 25, 35, -16, 60, -100};
+            int[] numbers = { 20, 30, 25, 35, -16, 60, -100 };
 
-            /*
-            fixme - calculate sum of all array elements 
             int sum = 0;
-            for (?){
-                
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
             }
-            */
 
-            /*
-            fixme - calculate average value 
+            double average = Math.Round((double)sum / numbers.Length, 2);
+
             Console.WriteLine("Average value of the array elements is : " + average);
-            */
+            Console.ReadKey();
+
         }
     }
 }
--- csharp-basics/exercises/Arrays/Arrays/Exercise4/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise4/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Exercise4
 {
     class Program
     {
-        //TODO: Write a C# program to test if an array contains a specific value.
         private static void Main(string[] args)
         {
             int[] myArray =
@@ -18,13 +15,15 @@ namespace Exercise4
                 1456, 2265, 1457, 2456
             };
 
-            /*
-            fixme - check if contains number 1245 
-            for (?) {
-                ......
+            if (myArray.Contains(1456))
+            {
                 Console.WriteLine("Contains!");
             }
-            */
+            else
+            {
+                Console.WriteLine("Does not exist in this array");
+            }
+            Console.ReadKey();
         }
     }
 }
