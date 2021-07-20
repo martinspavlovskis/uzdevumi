944b6ed9169ac27db68922f9ded834102a8c8620
Martins Pavlovskis <martinspavlovskis@yahoo.com>
2021-07-20 16:55:02 +0300

    8/10

--- csharp-basics/exercises/Arrays/Arrays/Exercise2/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise2/Program.cs
@@ -3,11 +3,10 @@
 namespace Exercise2
 {
     class Program
-    {       
-        //TODO: Write a C# program to sum values of an array.
+    {
         private static void Main(string[] args)
         {
-            int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
             var sum = 0;
 
             Console.WriteLine("Please enter a min number");
@@ -15,15 +14,13 @@ namespace Exercise2
             Console.WriteLine("Please enter a max number");
             int maxNumber = int.Parse(Console.ReadLine());
 
-            /*
-            fixme
-            for (........) {
-            ........
+            for (int i = minNumber; i <= maxNumber; i++)
+            {
+                sum += i;
             }
-            */
 
             Console.WriteLine("The sum is " + sum);
             Console.ReadKey();
-            }
         }
+    }
 }
---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Arrays6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+
+            int[] arr2 = new int[arr.Length];
+
+            var rand = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(101);
+
+            }
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                arr2[i] = arr[i];
+
+            }
+
+            arr[arr.Length - 1] = -7;
+
+            Console.WriteLine("{0}", string.Join(", ", arr));
+            Console.WriteLine("{0}", string.Join(", ", arr2));
+
+            Console.ReadKey();
+
+        }
+    }
+}
