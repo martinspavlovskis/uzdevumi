0e8a8bf9f4a8522151add011d2e0c57fcabe2d21
Martins Pavlovskis <martinspavlovskis@yahoo.com>
2021-07-20 15:46:38 +0300

    9/10

--- csharp-basics/exercises/Loops/Loops/Exercise2/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise2/Program.cs
@@ -6,32 +6,20 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
-            int i, n;
-            
-            Console.WriteLine("Input number of terms : ");
 
-            n = Convert.ToInt32(Console.ReadLine());
-            /*
-            todo - complete loop to multiply i with itself n times, it is NOT allowed to use Math.Pow()
-            */
-            for (i = 0; i < n; i++)
+            int result = 1;
+            int power;
+
+            Console.WriteLine("Input power : ");
+            power = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= power; i++)
             {
-                Console.WriteLine(Pow(i,n));
+                result *= i;
             }
-
+            Console.WriteLine(result);
             Console.ReadKey();
 
-        }
-
-        static int Pow(int number, int times)
-        {
-            var a = number;
-            for (var i = times; i > 1; i--)
-            {
-                a *= number;
-            }
-
-            return a;
         }
     }
 }
---
+++ csharp-basics/exercises/Loops/Loops/Loops5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Loops5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word1;
+            string word2;
+            
+            Console.WriteLine("Please enter your first word:");
+            word1 = Console.ReadLine();
+            Console.WriteLine("Please enter your second word:");
+            word2 = Console.ReadLine();
+
+            int wordLength = word1.Length + word2.Length;
+
+            Console.Write(word1);
+
+            for (int i = 0; i < 30 - wordLength; i++)
+            {
+                Console.Write(".");
+            }
+            Console.Write(word2);
+            Console.ReadKey();
+        }
+    }
+}
