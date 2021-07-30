3ece3164ce1468ebe2810bf9ed9240805ac21fb2
Martins Pavlovskis <martinspavlovskis@yahoo.com>
2021-07-30 19:11:57 +0300

    target framework change

--- csharp-basics/exercises/Polymorphism/Persons/Program.cs
+++ csharp-basics/exercises/Polymorphism/Persons/Program.cs
@@ -6,10 +6,7 @@ namespace Persons
     {
         static void Main(string[] args)
         {
-            /*
-             * - Create an instant of class Student and an instant of class Employee with proper values for the attributes.
-             * - Display the content of each class using Display() method.
-             */
+            Console.Write("dsffds");
         }
     }
 }
